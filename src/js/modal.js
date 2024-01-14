3974bea5cc832e11a1d6026047c0b64117edaa2a
zaleskiy88 <zaleskiy88@gmail.com>
2024-01-14 19:03:50 +0000

    added adaptive images for tablet&mobile versions, 2x 3x

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,12 +4,12 @@ const refs = {
   modal: document.querySelector('[data-modal]'),
   body: document.querySelector('body'),
 };
+console.log(refs.buttons);
+//const headerBtn = refs.buttons[0];  *//////TEMPORARY
+const aboutBtn = refs.buttons[0];
+const pricesBtn = refs.buttons[1];
 
-const headerBtn = refs.buttons[0];
-const aboutBtn = refs.buttons[1];
-const pricesBtn = refs.buttons[2];
-
-headerBtn.addEventListener('click', toggleModal);
+//headerBtn.addEventListener('click', toggleModal);    /////TEMPORARY
 aboutBtn.addEventListener('click', toggleModal);
 pricesBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -18,6 +18,7 @@ document.addEventListener('keydown', closeOnEsc);
 function toggleModal(e) {
   refs.modal.classList.toggle('is-hidden');
   refs.body.classList.toggle('no-scroll');
+  console.log('first');
 }
 
 function closeOnEsc(e) {
